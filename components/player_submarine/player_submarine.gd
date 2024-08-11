b3846f1fbcfccbf2ef6efe36dd86d9e8192ed365
Duhamel <duhamelgm@gmail.com>
2024-08-10 20:54:12 -0400

    Mejor movimiento

--- components/player_submarine/player_submarine.gd
+++ components/player_submarine/player_submarine.gd
@@ -1,19 +1,27 @@
 class_name PlayerSubmarine
 extends CharacterBody2D
 
+@export var max_steering = 0.3
+@export var auto_breaking_force = 0.2
 @export var speed = 400
 @onready var camera = $Camera2D
 @onready var submarine_flashlight = $SubmarineFlashlight
 
-func get_input():
-	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = input_direction * speed
+var stering = Vector2.ZERO
+
+func get_input(delta):
+	var input_direction = Input.get_vector("Left", "Right", "Up", "Down") / (10000 * delta)
+	stering += input_direction
+	stering = stering.lerp(Vector2.ZERO, auto_breaking_force * delta)
+	stering = stering.limit_length(max_steering)
+
+	velocity = stering * speed
 
 	if(Input.is_action_pressed("Flashlight")):
 		submarine_flashlight.turn_on()
 	else:
 		submarine_flashlight.turn_off()
 
-func _physics_process(_delta):
-	get_input()
+func _physics_process(delta):
+	get_input(delta)
 	move_and_slide()
