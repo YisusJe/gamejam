2f785116387b02b6285bb742302cd5f920ffff0d
Duhamel <duhamelgm@gmail.com>
2024-08-10 20:33:38 -0400

    un cristo mas inteligente

--- components/enemy/enemy.gd
+++ components/enemy/enemy.gd
@@ -4,23 +4,34 @@ extends CharacterBody2D
 @export var speed = 100
 @onready var area = $Area2D
 
-var is_illuminated = false
+var is_attacking = true
+var timer = null
 
 func _ready():
 	area.monitoring = true
 	area.connect("area_entered", on_light_entered)
-	area.connect("area_exited", on_light_exited)
 
 func _physics_process(_delta):
 	var raw_velocity = position.direction_to(player_submarine.position) * speed
-	velocity = -raw_velocity if is_illuminated else raw_velocity # Move in opposite direction if illuminated
+	velocity = raw_velocity if is_attacking else -raw_velocity # Move in opposite direction if illuminated
 
 	move_and_slide()
 
-func on_light_entered(_area):
-	is_illuminated = true
+func attack_player():
+	is_attacking = true
+	timer.queue_free()
+	timer = null
 	pass
 
-func on_light_exited(_area):
-	is_illuminated = false
-	pass
+func on_light_entered(_area):
+	is_attacking = false
+
+	if(timer):
+		return
+
+	timer = Timer.new()
+	timer.wait_time = 2.0 # 1 second
+	timer.one_shot = true # don't loop, run once
+	timer.autostart = true # start timer when added to a scene
+	timer.connect("timeout", attack_player)
+	add_child(timer)
