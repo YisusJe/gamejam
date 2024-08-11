60ba37e3358bc610259757bb911850b36cdf7996
Jesus David <jesusdpch00@gmail.com>
2024-08-11 16:50:14 -0400

    feat: Add FlashlightTimer component for controlling flashlight duration

---
+++ components/submarine_flashlight/FlashlightTimer.gd
@@ -0,0 +1,12 @@
+extends Timer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#wait_time = flashlightDuration
+	autostart = false
+	one_shot = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
