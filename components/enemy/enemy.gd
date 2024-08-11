5b627c8b723c0e319e9b7112f0909cd388bf4b41
Duhamel <duhamelgm@gmail.com>
2024-08-11 11:20:42 -0400

    Cristo inteligencia superior

--- components/enemy/enemy.gd
+++ components/enemy/enemy.gd
@@ -1,36 +1,282 @@
 extends CharacterBody2D
 
 @export var player_submarine : PlayerSubmarine
-@export var speed = 100
+@export var player_attacking_distance = 150
+@export var player_hunting_distance = 200
+@export var player_running_distance = 400
 @onready var area = $Area2D
+@onready var sprite = $Sprite2D
+@onready var ray_cast_up = $RayCastUp
+@onready var ray_cast_down = $RayCastDown
+@onready var ray_cast_left = $RayCastLeft
+@onready var ray_cast_right = $RayCastRight
 
 var is_attacking = true
 var timer = null
+var movement_mode : MovementModes = MovementModes.Hunting;
+
+enum MovementModes {
+	Hunting,
+	Attacking,
+	Running
+}
+
+var rng = RandomNumberGenerator.new()
+
+var rays = []
+
+var radius = 1
+var number_of_rays = 16
 
 func _ready():
 	area.monitoring = true
 	area.connect("area_entered", on_light_entered)
 
+	var step = 2 * PI / number_of_rays
+
+	for i in range(number_of_rays):
+		var ray = Vector2(1, 0).rotated(step * i)
+		rays.append(ray)
+
+func get_dot_product_percentage(dot_product):
+	return (dot_product + 1) / 2
+
+func linear_interpolate(a, b, percentage):
+	return a + ((b - a) * percentage)
+
+func clamp_weight(weight):
+	return clamp(weight, 0.0, 1.0)
+
+func update_weights_by_collisions(rays_frame):
+	var colliding_vectors = []
+	var output = []
+	
+	if (ray_cast_up.is_colliding()):
+		colliding_vectors.append(Vector2(0, -1.0))
+	if (ray_cast_down.is_colliding()):
+		colliding_vectors.append(Vector2(0, 1.0))
+	if (ray_cast_left.is_colliding()):
+		colliding_vectors.append(Vector2(-1.0, 0.0))
+	if (ray_cast_right.is_colliding()):
+		colliding_vectors.append(Vector2(1.0, 0.0))
+
+	if (colliding_vectors.size() <= 0):
+		return rays_frame
+		
+	for ray in rays_frame:
+		var weight = ray['weight']
+
+		for colliding_ray in colliding_vectors:
+			var dot_product = ray['ray'].dot(colliding_ray)
+			
+			# Those that are pointing to the collision vector will have a weight of 0, as we can't move there
+			if (dot_product > 0.5):
+				weight = 0
+			else:
+				continue
+
+			break
+
+		output.append({
+			'ray': ray['ray'],
+			'weight': weight
+		})
+
+	return output
+
+func update_weights_running(rays_frame, direction_to_player):
+	var output = []
+
+	for ray in rays_frame:
+		var dot_product_percentage = get_dot_product_percentage(ray['ray'].dot(direction_to_player))
+		var weight = ray['weight']
+
+		# If ray points to player then it will reduce score by a maximum of 60% from 100%
+		var weight_loss = linear_interpolate(0.6, 1, 1 - dot_product_percentage)
+
+		output.append({
+			'ray': ray['ray'],
+			'weight': clamp_weight(weight * weight_loss)
+		})
+
+	return output
+
+func update_weights_hunting(rays_frame, direction_to_player, closer_than_attacking_distance, further_than_hunting_distance):
+	var output = []
+
+	for ray in rays_frame:
+		var dot_product_percentage = get_dot_product_percentage(ray['ray'].dot(direction_to_player))
+		var weight = ray['weight']
+
+		# If ray points to player direction then it will reduce score by a maximum of 60% from 100%
+		var weight_loss = linear_interpolate(0.8, 1, 1 - dot_product_percentage)
+
+		# Prioritize lateral movement
+		if dot_product_percentage > 0.4 && dot_product_percentage < 0.6:
+			weight_loss = 1.0
+		# Move closer to player if further away than hunting distance
+		if further_than_hunting_distance:
+			weight_loss = linear_interpolate(0.5, 1, dot_product_percentage)
+
+		output.append({
+			'ray': ray['ray'],
+			'weight': clamp_weight(weight * weight_loss)
+		})
+
+	return output
+
+
+func update_weights_attacking(rays_frame, direction_to_player):
+	var output = []
+
+	for ray in rays_frame:
+		var dot_product_percentage = get_dot_product_percentage(ray['ray'].dot(direction_to_player))
+		var weight = ray['weight']
+
+		# If ray doesn't point to intended direction then it will reduce score by a maximum of 0% from 100%
+		var weight_loss = linear_interpolate(0.0, 1, dot_product_percentage)
+
+		output.append({
+			'ray': ray['ray'],
+			'weight': clamp_weight(weight * weight_loss)
+		})
+
+	return output
+
+func update_weights_on_intended_direction(rays_frame, direction):
+	var output = []
+
+	for ray in rays_frame:
+		var dot_product_percentage = get_dot_product_percentage(ray['ray'].dot(direction))
+		var weight = ray['weight']
+
+		# If ray doesn't point to intended direction then it will reduce score by a maximum of 80% from 100%
+		var weight_loss = linear_interpolate(0.8, 1, dot_product_percentage)
+
+		output.append({
+			'ray': ray['ray'],
+			'weight': clamp_weight(weight * weight_loss)
+		})
+
+	return output
+
+func update_weights_on_previous_best_ray(rays_frame, best_ray):
+	var output = []
+
+	for ray in rays_frame:
+		var dot_product_percentage = get_dot_product_percentage(ray['ray'].dot(best_ray))
+		var weight = ray['weight']
+
+		# If ray doesn't point to intended direction then it will reduce score by a maximum of 80% from 100%
+		var weight_loss = linear_interpolate(0.8, 1, dot_product_percentage)
+
+		output.append({
+			'ray': ray['ray'],
+			'weight': clamp_weight(weight * weight_loss)
+		})
+
+	return output
+
+var previous_best_ray = null
+var previous_rays = []
+
+# func _draw():
+# 	for ray in previous_rays:
+# 		var distance = ray['ray'] * ((100 * ray['weight']))
+
+# 		draw_line(Vector2(0.0, 0.0), distance, Color.GREEN, 1.0)
+
+# 	draw_line(Vector2(0.0, 0.0), previous_best_ray * 100, Color.RED, 1.0)
+
 func _physics_process(_delta):
-	var raw_velocity = position.direction_to(player_submarine.position) * speed
-	velocity = raw_velocity if is_attacking else -raw_velocity # Move in opposite direction if illuminated
+	var direction_to_player = position.direction_to(player_submarine.position)
+	var distance_to_player = (player_submarine.position - position).length()
+	var direction = direction_to_player
+	var speed = 10 + rng.randf_range(0.0, 10.0)
+
+	var rays_frame = []
+
+	for ray in rays:
+		rays_frame.append({
+			'ray': ray,
+			'weight': 1.0
+		})
+
+	rays_frame = update_weights_by_collisions(rays_frame)
+	
+	if (movement_mode == MovementModes.Hunting):
+		if (distance_to_player < player_hunting_distance):
+			if (rng.randf_range(0.0, 1.0) > 0.99):
+				movement_mode = MovementModes.Attacking
+		if (previous_best_ray):
+			rays_frame = update_weights_on_previous_best_ray(rays_frame, previous_best_ray)
+		if (distance_to_player > player_hunting_distance):
+			rays_frame = update_weights_on_intended_direction(rays_frame, direction)
+
+		rays_frame = update_weights_hunting(rays_frame, direction_to_player, distance_to_player < player_attacking_distance, distance_to_player > player_hunting_distance)
+
+		if (distance_to_player > player_hunting_distance):
+			speed += rng.randf_range(50.0, 100.0)
+
+		var percentage_speed = 1 - ease(distance_to_player / float(player_hunting_distance), -0.2)
+		speed += percentage_speed * 200
+	elif (movement_mode == MovementModes.Attacking):
+		rays_frame = update_weights_attacking(rays_frame, direction_to_player)
+		var percentage_speed = 1 - ease((distance_to_player / float(player_attacking_distance)), 0.4)
+		speed += rng.randf_range(50.0, 100.0)
+		speed += percentage_speed * 2000
+	elif (movement_mode == MovementModes.Running):
+		rays_frame = update_weights_on_intended_direction(rays_frame, -direction_to_player)
+		rays_frame = update_weights_running(rays_frame, direction_to_player)
+		speed = 500
+
+	var max_weight = 0.0
+	for ray in rays_frame:
+		if ray['weight'] >= max_weight:
+			max_weight = ray['weight']
+
+	var best_rays = rays_frame.reduce(func(out, val): 
+		if val['weight'] != max_weight:
+			return out
+
+		out.append(val)
+		return out
+	, [])
+	var best_ray = best_rays.pick_random()['ray']
+
+	if(previous_best_ray):
+		previous_best_ray = best_ray
+	else:
+		previous_best_ray = best_ray
+	previous_rays = rays_frame
+
+	queue_redraw()
+
+	velocity = velocity.lerp(speed * previous_best_ray, 0.03)
+
+	if movement_mode == MovementModes.Running:
+		sprite.flip_v = true if direction.x > 0 else false
+		sprite.look_at(global_position - direction)
+	else:
+		sprite.flip_v = true if direction.x < 0 else false
+		sprite.look_at(global_position + direction)
 
 	move_and_slide()
 
 func attack_player():
-	is_attacking = true
+	movement_mode = MovementModes.Hunting
 	timer.queue_free()
 	timer = null
-	pass
 
 func on_light_entered(_area):
-	is_attacking = false
+	previous_best_ray = null
+	movement_mode = MovementModes.Running
 
 	if(timer):
 		return
 
 	timer = Timer.new()
-	timer.wait_time = 2.0 # 1 second
+	timer.wait_time = 3.0 # 1 second
 	timer.one_shot = true # don't loop, run once
 	timer.autostart = true # start timer when added to a scene
 	timer.connect("timeout", attack_player)
--- scripts/camera.gd
+++ scripts/camera.gd
@@ -4,11 +4,8 @@ func _process(delta):
 	var window_size = DisplayServer.window_get_size()
 	var mouse_pos = get_global_mouse_position()
 
-	print(window_size.x)
-	var new_offset = Vector2.ZERO
-	new_offset.x = (mouse_pos.x - global_position.x) / (window_size.x / 2) * 100
-	new_offset.y = (mouse_pos.y - global_position.y) / (window_size.y / 2) * 100
+    var new_offset = Vector2.ZERO
+    new_offset.x = (mouse_pos.x - global_position.x) / (window_size.x / 2) * 100
+    new_offset.y = (mouse_pos.y - global_position.y) / (window_size.y / 2) * 100
 
-	offset = offset.lerp(new_offset, delta * 5)
-
-	print(offset.x)
+    offset = offset.lerp(new_offset, delta * 5)
