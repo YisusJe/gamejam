ef85412dddcc24225eb54a9decc3541660af521a
Duhamel <duhamelgm@gmail.com>
2024-08-12 22:16:19 -0400

    Cristo ve para 4 lados mas

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,5 +1,11 @@
 extends Camera2D
 
+@export var random_strength : float = 30.0
+@export var shake_fade : float = 5.0
+
+var rng = RandomNumberGenerator.new()
+var shake_strength = 0
+
 func _process(delta):
 	var window_size = DisplayServer.window_get_size()
 	var mouse_pos = get_global_mouse_position()
@@ -8,4 +14,15 @@ func _process(delta):
 	new_offset.x = (mouse_pos.x - global_position.x) / (window_size.x / 2) * 100
 	new_offset.y = (mouse_pos.y - global_position.y) / (window_size.y / 2) * 100
 
-	offset = offset.lerp(new_offset, delta * 5)
+	if shake_strength > 0:
+		shake_strength = lerpf(shake_strength, 0, shake_fade * delta)
+
+		offset = random_offset()
+
+	new_offset = new_offset.lerp(new_offset, delta * 5)
+
+func shake_camera():
+	shake_strength = random_strength
+
+func random_offset():
+	return Vector2(rng.randf_range(-shake_strength, shake_strength), rng.randf_range(-shake_strength, shake_strength))
